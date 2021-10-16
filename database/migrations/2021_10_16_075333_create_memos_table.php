c58094fc97b4ac7fe49fed5ed3641ff555b12397
ttakuma <ttanaka@fourmix.co.jp>
2021-10-16 16:55:56 +0900

    環境構築

---
+++ database/migrations/2021_10_16_075333_create_memos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->id('user_id')->nullable();
+            $table->uuid('folder_id')->nullable();
+            $table->string('title');
+            $table->text('contents');
+            $table->boolean('is_public')->default(false);
+            $table->boolean('is_archive')->default(false);
+            $table->timestamps();
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
